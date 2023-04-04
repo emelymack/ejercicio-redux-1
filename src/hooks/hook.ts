a840d10de68cb9929f369e346fabe10f0f0fb63d
emelymack <emelymack@gmail.com>
2023-04-03 23:15:47 -0300

    Implemento Netlify

--- src/hooks/hook.ts
+++ src/hooks/hook.ts
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
 import type { RootState, AppDispatch } from '../redux/store'
 
+/* esto es para usar los hooks ya tipados y no tener q tiparlos cada vez! */
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -11,6 +11,7 @@ export const store = configureStore({
   }
 })
 
+/* para tipar los objetos de estado y dispatch */ 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
